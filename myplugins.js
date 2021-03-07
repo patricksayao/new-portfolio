5db117c2fe36edd42f99e02718b8416b13ce5d61
patricksayao <77041730+patricksayao@users.noreply.github.com>
2021-03-07 16:21:14 +0800

    Add files via upload

---
+++ myplugins.js
@@ -0,0 +1,33 @@
+
+// hide welcome
+setInterval(function() {
+  $(".letter-1").animate({opacity: 1}, 800).delay(2000).animate({opacity: 0}, 500).delay(2000).animate({opacity: 1}, 800);
+}, 100);
+
+setInterval(function() {
+  $(".letter-2").animate({opacity: 1}, 800).delay(2000).animate({opacity: 0}, 500).delay(2000).animate({opacity: 1}, 800);
+}, 200);
+
+setInterval(function() {
+  $(".letter-3").animate({opacity: 1}, 800).delay(2000).animate({opacity: 0}, 500).delay(2000).animate({opacity: 1}, 800);
+}, 400);
+
+setInterval(function() {
+  $(".letter-4").animate({opacity: 1}, 800).delay(2000).animate({opacity: 0}, 500).delay(2000).animate({opacity: 1}, 800);
+}, 600);
+
+setInterval(function() {
+  $(".letter-5").animate({opacity: 1}, 800).delay(2000).animate({opacity: 0}, 500).delay(2000).animate({opacity: 1}, 800);
+}, 800);
+
+setInterval(function() {
+  $(".letter-6").animate({opacity: 1}, 800).delay(2000).animate({opacity: 0}, 500).delay(2000).animate({opacity: 1}, 800);
+}, 1000);
+
+setInterval(function() {
+  $(".letter-7").animate({opacity: 1}, 800).delay(2000).animate({opacity: 0}, 500).delay(2000).animate({opacity: 1}, 800);
+}, 1200);
+
+// setInterval(function() {
+//   $("#bang-2").style.msTransform = "rotate(11deg)";
+// }, 100);
